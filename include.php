09829efb2f59194f8870b933602e3935c684ff21
Roscenzura-com <59208151+Roscenzura-com@users.noreply.github.com>
2022-10-30 23:37:23 +0300

    Update include.php

--- include.php
+++ include.php
@@ -3,6 +3,7 @@
  
 $url=$_SERVER['REQUEST_URI']; // Если ддосят только главную поставьте $url='/'; для экономии ресурсов. 
 
+//var_dump($url);
 $status=false;
 
 // Это можно убрать, если переменная $_SERVER['REMOTE_ADDR'] содержит IP пользователя, а не Cloudflare
@@ -11,173 +12,141 @@ if (isset($_SERVER['HTTP_CF_CONNECTING_IP']))
  	$_SERVER['REMOTE_ADDR']=$_SERVER['HTTP_CF_CONNECTING_IP'];
 }
 
+if (isset($_COOKIE['cf_clearance']))  // Кука прохождения капчи, сработает только если IP был добавлен в фаерволл Cloudflare  
+{	 
+	$cf_clearance=$_COOKIE['cf_clearance'];
+}
+else
+{
+	$cf_clearance=false;
+}
+ 
+
 if (is_file(__DIR__.'/ban/'.$_SERVER['REMOTE_ADDR'])) // ip в бане
 {
-	include(__DIR__.'/config.php');
+	$status='ban';
 	
-	if (!is_file(__DIR__.'/captcha_ip/'.$_SERVER['REMOTE_ADDR']))	
-	{	
-		$status='ban'; 	
+	include(__DIR__.'/config.php');
+	include(__DIR__.'/Cloudflare.class.php');
+	
+	$cf = new Cloudflare($config['CF']);
 
-		include(__DIR__.'/cloudflare.class.php');
-		
-		$cf = new Cloudflare($configCF);
-		
-		$cf->ip=$_SERVER['REMOTE_ADDR'];
-		$cf->country= $_SERVER['HTTP_CF_IPCOUNTRY'];
-		$cf->countries=$configCF['countries']; 
-		
-		// авторизация на Cloudflare
-		if ( !$cf->auth($configCF['email'], $configCF['key'], $configCF['zone']) )
-		{
-			exit ('Не получилось подключиться к Cloudflare, проверьте настройки подключения.'); 
-		}
-		
-		$desc='antiddos '.$cf->country.' '.date('Y-m-d'); // комментарий для Cloudflare
-		
-		$cf->counterCountries(); // Счетчик по странам, папка countries, в админке: Cloudflare->География ботов
-
-		if ( !$cf->checkCountry() ) // Не целевой трафик
-		{
-			$r=$cf->addcountry($desc, 'challenge'); // доступные способы блокировки: challenge (каптча), block (блок), js_challenge (ява скриптовая каптча, иногда обходится ддос ботами)
-		
-			if (!$r['success']) $r=$cf->addrange($desc, 'challenge'); // если не получилось добавить страну, баним по диапазону
+	if (!is_file(__DIR__.'/captcha_ip/'.$cf->ip)) // Капчу еще не проходили, то есть забанены первый раз	
+	{		
+		if ($cf_clearance && $cf->getId('ip')) // Кука Cloudflare после прохождения каптчи 
+		{		
+			$cf->captchaTrue(); // Удаляет IP из бана
+			
+			$status='captcha_true';	
 		}
 		else
 		{
-			$cf->addip($desc, 'challenge'); // Целевой трафик блокируем точечно
-		}
-		/*
-		elseif ($cf->country=='RU') // Россию блокируем точечно 
-		{
-			$cf->addip($desc, 'js_challenge');	
-		}
-		else $cf->addrange($desc, 'challenge'); 
-		*/
-		
-		$cf->close();
-				
-		$cf->captchaPass(); // IP прошел проверку Cloudflare, откладываем его в папку captcha_ip
+			if ( !$cf->auth() ) // авторизация на Cloudflare
+			{
+				exit ('Не установлены настройки подключения к Cloudflare.'); 
+			}
+			
+			$date=date('Y-m-d');
+			$desc='antiddos 2.0 '.$cf->country; // комментарий для Cloudflare
+						
+			$r=$cf->addip($desc.' '.$date, $cf->blockMethod('ip') );  
+			
+			$cf->counterGeoBots(); // Счетчик по странам, папка countries, в админке: Cloudflare->География ботов
+	
+			if ( !$cf->checkCountry() ) // Страна не целевого трафика
+			{
+				if ( !$cf->addcountry($desc.' block country '.$date,  $cf->blockMethod('country') ) )
+				{
+					$r=$cf->addrange($desc.' block range '.$date,  $cf->blockMethod('ip_range') ); // если не получилось добавить страну, баним диапазон  
+				}
+			}
+		}	
 	}
-	else
+	else // Если уже проходили капчу ранее, то есть были разбанены 
 	{
-		$status='captcha_true';			
-			
-		///var_dump( $cf->getRules('ip'));	
-		
-		// Если ддосер вручную прошел капчу
-		
-		$config['limit']=$configCF['limit']; // Переопределяем настройки
-		
-		include(__DIR__.'/antiddos.class.php');
-		$antiddos = new Antiddos($config, $_SERVER['REMOTE_ADDR']);
-
-		if ($antiddos->excessLimit())  // Проверка на превышение лимита
+		if ( !$cf->auth() ) // авторизация на Cloudflare
 		{
-			$status='ban';
-			
-			include(__DIR__.'/cloudflare.class.php');
-			$cf = new Cloudflare($configCF);
-			$cf->auth($configCF['email'], $configCF['key'], $configCF['zone']);
-			
-			$cf->ip=$_SERVER['REMOTE_ADDR'];
-			$cf->country= $_SERVER['HTTP_CF_IPCOUNTRY'];
+			exit ('Не установлены настройки подключения к Cloudflare.'); 
+		}
 	
-			if ($id=$cf->getId('ip'))
+		if ($id=$cf->getId('ip')) // Проверяем есть ли такой IP в правилах
+		{
+			// Баним жестко без проверки на каптчу, потому что пользователь уже проходил каптчу и снова превысил лимит
+			$r=$cf->changeRuleMode($id, "block", 'antiddos 2.0 change rule '.date('Y-m-d') ); // .date('Y-m-d')	
+
+			if (!$r) 		 
 			{
-				$r=$cf->changeRuleMode($id, 'block'); // Баним окончательно 
-				
-				$cf->save('ip', $cf->ip, 'block');
-			}
-			else
-			{
-				$r=$cf->addip('The user passed the captcha and exceeded the limit of requests '.$cf->country, 'block');
+				$cf->delRuleById($id);
 			}
 		}
-		
+		else
+		{
+			$r=$cf->addip('antiddos 2.0: user passed the captcha and exceeded the limit of requests '.$cf->country.' '.date('Y-m-d'), 'block'); // 
+		}
+	}
 	
-	}	
-
-	exit('Вы временно забанены, попробуйте зайти попозже');
-		
-	if (!isset($testAntiddos)) unset($config, $cf);
+	$cf->close();
+	
+	if ($status=='ban')
+	{
+		echo 'Вы временно забанены, попробуйте зайти попозже. '.($config['adminEmail'] ? 'Если произошла ошибка, напишите на Email '.$config['adminEmail'] : '');
+		exit();
+	}
 }
-elseif (!is_file(__DIR__.'/white/'.$_SERVER['REMOTE_ADDR']) && $_SERVER['REQUEST_URI']==$url )
-{	
-	include(__DIR__.'/config.php');
-	include(__DIR__.'/antiddos.class.php');
+elseif (is_file(__DIR__.'/white/'.$_SERVER['REMOTE_ADDR']))
+{
+	$status='white';
 
-	$antiddos = new Antiddos($config, $_SERVER['REMOTE_ADDR']);
-	
-	if (isset($_SERVER['HTTP_CF_IPCOUNTRY'])) $antiddos->country_code=$_SERVER['HTTP_CF_IPCOUNTRY'];
-	
-	$attack_mode=false;
-	if ($config['limit_attack_mode']!=0) 
-	{
-		$ctime=date("h.i");
-		$file_count=__DIR__.'/count_attack.txt';
-		
-		if (!$str=file_get_contents($file_count)) $str=':';
-		list($time, $count)=explode(':',$str);
-	
-		if ($ctime!=$time) 
-		{
-			$count='1';
-		}
-		else $count+=1;
-	
-		if ( $count > $config['limit_attack_mode'] ) $attack_mode=true;
-		
-		if (!file_put_contents($file_count, $ctime.':'.$count)) exit('Установите права на запись для файла '.__DIR__.'/count_attack.txt');
-	}
-	
+}
+elseif ( $_SERVER['REQUEST_URI']==$url  )
+{	
+	 
+	include(__DIR__.'/config.php');
+	include(__DIR__.'/Antiddos.class.php');
+
+	$antiddos = new Antiddos($config); // $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_CF_IPCOUNTRY']
 	
 	if ($search=$antiddos->isBot()) // Проверка на поискового бота
 	{			
 		if ($antiddos->checkBot($search)) // Проверяем подлинность бота. Если проверка не нужна, просто переопределяем переменную $config['search_hosts']=array()
 		{ 
 			$status='search bot';
-			$antiddos->addWhitelist('search bot', $config['whitetime'] ); // Добавляем в белый список 
+			$antiddos->addWhitelist('search bot'); // Добавляем в белый список 
 		}
 		else 
 		{
 			$status='fake bot';
-			$antiddos->inlog('badbot');
+			$antiddos->inlog('badbot', $antiddos->getIpHost() );
 			
-			$antiddos->addBanlist('fake bot '.$antiddos->desc, $config['bantime']); // В черный список
+			$antiddos->addBanlist('fake bot '.$antiddos->getIpHost(), $config['bantime']); // В черный список
 		} 			
 	}
-	elseif($attack_mode)
+	elseif( $antiddos->attackModeSwitcher()=='On' )
 	{
-		$antiddos->attackMode();
+		if ($antiddos->excessLimitAttackMode()) $antiddos->addBanlist('attack mode limit exceeded', $config['bantime']);
 		
-		echo 'На сайт идет ддос, пожалуйста, не обновляйте страницу, чтобы не попасть в бан. Попробуйте зайти попозже.';
+		echo '<h1>На сайт идет ддос, пожалуйста, не обновляйте страницу, чтобы не попасть в бан. Попробуйте зайти попозже.</h1>';
 		exit;
-	
 	}	
-	/*
-	elseif (isset($config['referer'][0]) && $r=$antiddos->goodReferer()) // Реферер
+	elseif ($r=$antiddos->goodReferer()) // Реферер
 	{
 		$antiddos->inlog('referer');
 		
-		$antiddos->addWhitelist($r.' good referer', $config['referertime'] );
+		// $antiddos->addWhitelist($r.' good referer', $config['referertime'] );
 	}
-	*/
-	/*
-	elseif (!$config['limit']) // Если лимит запросов не задан, добавляем все IP в бан. Для удаления из бана пользователь должен будет пройти каптчу
-	{
-		$antiddos->addBanlist('proactive ban', $config['proactivetime']); // Баним на час
-	}
-	*/
-	elseif ($antiddos->excessLimit())  // Проверка на превышение лимита
+	elseif ($antiddos->excessLimitBan())  // Проверка на превышение лимита
 	{
  		$status='ban';
 		$antiddos->addBanlist('limit exceeded', $config['bantime']);
 	}
-	
-	if (!isset($testAntiddos)) unset($config, $antiddos);
+	elseif ($antiddos->excessLimitWarn())
+	{
+		$status='warn';
+		echo '<h1>С вашего IP слишком много заходов в минуту. Пожалуйста, не обновляйте страницу в течении минуты, чтобы не попасть в бан.</h1>';
+		exit;
+	}
+		
+//	echo $antiddos->counter;
 }
-else $status='white';
-
-
- 
+	 
+// exit($status);
