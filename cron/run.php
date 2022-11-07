357e5358fcced5c7c6d3fac09208fec2c0a53c35
Roscenzura-com <59208151+Roscenzura-com@users.noreply.github.com>
2022-11-07 13:43:02 +0300

    Update run.php

--- cron/run.php
+++ cron/run.php
@@ -1,4 +1,5 @@
 <?PHP
+session_start();
 ini_set('register_argc_argv', 1);
 
 include('../config.php');
@@ -8,7 +9,9 @@ $admin= new Admin($config);
 
 if (isset($_GET['action']))
 {
-	if ($_SERVER['HTTP_USER_AGENT']!='antiddos')
+	if (isset($_SESSION['pass'])) $login= ($_SESSION['pass']==md5($config['admin']['pass']) );
+	
+	if ($_SERVER['HTTP_USER_AGENT']!='antiddos' && !$login )
 	{
 		$admin->cronLog($_GET['action'].' ошибка, задание крон было вызвано без флага --user-agent, смотрите инструкцию на форуме ddosforum.com' ) ;
 	}
@@ -18,7 +21,7 @@ elseif (isset($argv[1]))
 	$_GET['action']=$argv[1];
 }
 
-
+$r='noaction';
 if ($_GET['action']=='clearcount')
 {
 	if ($admin->cronTimer('counter')) $r=$admin->clearList('count');
@@ -29,9 +32,11 @@ elseif ($_GET['action']=='clearban')
 }
 else exit();
 
-$admin->cronLog($_GET['action'].' '.($r ? 'выполнен' : 'были ошибки') ) ;
+if ($r!=='noaction')
+{
+ $admin->cronLog( $_GET['action'].' '.($r ? 'выполнен' : 'были ошибки') ) ;
 
-
+}
 
 //file_put_contents(__DIR__.'/log.txt', $argv[0].' '.$argv[1], FILE_APPEND);
 //var_dump($r);
