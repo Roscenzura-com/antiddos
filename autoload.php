516e2de5db0bd7daf6f65c634faf7841429a1328
Roscenzura-com <59208151+Roscenzura-com@users.noreply.github.com>
2022-12-11 03:27:02 +0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,7 +1,5 @@
 <?PHP
 spl_autoload_register(function ($class) {
-//var_dump(substr(strrchr('\\'.$class, "\\" ), 1)  . ".class.php");
-    //include(substr(strrchr('\\'.$class, "\\" ), 1)  . ".class.php");
 	include(__DIR__.'/'.$class. ".class.php");
 });
 ?>
