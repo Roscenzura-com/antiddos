c5701ffd0c6ac97bfb0c97a16243e7c12ed0877d
Roscenzura-com <59208151+Roscenzura-com@users.noreply.github.com>
2022-12-11 03:14:05 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 $config=[];
 $config['admin']['pass']='test'; // Пароль админки
-$config['adminEmail']=''; // Емейл для связи (в случае ошибочной блокировки)
+$config['adminEmail']='admin@ddosforum.com'; // Емейл для связи (в случае ошибочной блокировки)
 
 
-$config['bantime']=604800; // время блокировки (604800 неделя,  31536000 месяц, 3600 час) 
+$config['bantime']=81536000; // 604800 неделя,  31536000 месяц, 3600 час // 
 
 
 // Лимиты
-$config['limit']=8; // Количество заходов в минуту для добавления IP в бан. Убедитесь, что нет никаких скриптов, которые обращаются чаще (чат, например)
-$config['limit_warning']=6; // Количество заходов в минуту, после чего появляется предупреждение.  
+$config['limit']=13; // Количество заходов в минуту для добавления IP в бан. Убедитесь, что нет никаких скриптов, которые обращаются чаще (чат, например)
+$config['limit_warning']=11; // Количество заходов в минуту, после чего появляется предупреждение.  
 
 
 /////////
@@ -18,24 +18,21 @@ $config['limit_warning']=6; // Количество заходов в минут
 
 //$config['attack_mode']=false;
 $config['attack_mode']=true; // Для включения установите значение true, для отключения false.
-
-// Количество заходов со всех IP в минуту при которых включается режим "под атакой". */
-$config['limit_attack_mode']=53; // Рекомендуемое значение примерно 1/24 от среднего числа просмотров в сутки во избежании ложных срабатываний.   
+$config['attack_mode_timer']=300; 
+// Количество заходов со всех IP в минуту при которых включается режим "под атакой".  
+$config['limit_attack_mode']=32; // Рекомендуемое значение примерно 1/24 от среднего числа просмотров в сутки во избежании ложных срабатываний.   
 
 // Количество заходов в минуту для срабатывания бана в режиме "под атакой". Если выставить 1, то будут залетать все IP, кроме поисковых ботов.
 // Предполагается, что пользователь не будет обновлять страницу после того как увидит соответствующее предупреждение. Боты проигнорируют предупреждение и попадут в бан. 
-$config['limit_attack_mode_ban']=2; 
+$config['limit_attack_mode_ban']=3; 
 
 /////////
 ///////// 
 
 
-// Данные для генерации файла счетчика
-$config['url']=$_SERVER['REQUEST_URI'];  // счетчик для каждой отдельной страницы сайта
-// $config['url']= strstr(;$_SERVER['REQUEST_URI'], '?', true);  // счетчик для страниц без учета GET параметров
-
-$config['user_agent']=''; // счетчик не учитывает браузер пользователя
-// $config['user_agent']=$_SERVER['HTTP_USER_AGENT']; // счетчик учитывает браузер пользователя
+// Целевая ссылка
+$config['url']=$_SERVER['REQUEST_URI'];  // 
+$config['user_agent']='';
 
 
 
@@ -53,27 +50,33 @@ $config['user_agent']=''; // счетчик не учитывает браузе
 
 //$config['referer']=[$_SERVER['HTTP_HOST'], 'yandex.ru', 'google.com', 'google.ru'];
 $config['referer']=[]; // Рефереры для исключения, ддос боты обычно шлют пустые реферы 
-
 	
 $config['search_bots']=['Googlebot'=>'Google', 'yandex.com/bots'=>'Yandex', 'mail.ru'=>'mail.ru'];  // 'msn.com', 'bing.com'   
 $config['search_hosts']=['Google'=>['.googlebot.com', '.google.com'], 'Yandex'=>['.yandex.com', '.yandex.ru', '.yandex.net'], 'mail.ru'=>['.mail.ru'], 'msn.com'=>['.msn.com'], 'bing.com'=>['.msn.com'] ];	
 
 $config['cron']=
 [
-	'banlist'=>3600, // переодичность в секундах для обновления списка забаненных (удаления IP время бана которых истекло)
-	'counter'=>888
+	/*'banlist'=>3600,*/ // переодичность в секундах для обновления списка забаненных (удаления IP время бана которых истекло)
+	'counter'=>3600 
 ];
 
 
 //Cloudflare
 $config['CF']=
 [
-    'email'=>'',  // email вашего аккаунта в Cloudflare
-    'key'=>'', // ключ доступа (Global API Key), узнать можно на странице dash.cloudflare.com/profile
-    'zone'=>'', // ID домена в Cloudflare, есть во вкладке Overview
- 
-    'countries'=>['RU'=>1, 'UA'=>1, 'BY'=>1, 'KZ'=>1, 'LV'=>1, 'FI'=>1, 'GE'=>1, 'IL'=>1, 'KG'=>1, 'PL'=>1, 'TJ'=>1, 'UZ'=>1], // страны целевого трафика
-
-    'block_method'=>['country'=>'managed_challenge', 'ip_range'=>'managed_challenge', 'ip'=>'managed_challenge']  // методы блокировки для стран нецелевого трафика, диапазона IP и IP
+	'email'=>'',  // email вашего аккаунта в Cloudflare
+	'key'=>'', // Узнать можно на странице dash.cloudflare.com/profile, Global API Key
+	'zone'=>'', // ID домена в Cloudflare, есть во вкладке Overview 
+	
+	'countries'=>['RU'=>1, 'UA'=>1, 'BY'=>1, 'KZ'=>1, 'LV'=>1, 'FI'=>1, 'GE'=>1, 'IL'=>1, 'KG'=>1, 'PL'=>1, 'TJ'=>1, 'UZ'=>1], // страны целевого трафика
+	'target'=>['ip'=>'ip', 'ip6'=>'ip', 'ip_range'=>'ip_range', 'country'=>'country'],
+	'notes'=>[
+				'ip'=>'antiddos, %country%, %time%, %url%',	
+				'ip_range'=>'antiddos, %country%, %time%',
+				'country'=>'antiddos, country, %ip%, %time%',
+				'default'=>'antiddos, %time%'
+			 ],
+	
+	 // методы блокировки: challenge (каптча), block (запрет доступа), js_challenge (ява скриптовая проверка, без ввода каптчи, иногда обходится ддос ботами), managed_challenge (каптча только для "подозрительных" IP)
+	'block_method'=>['country'=>'managed_challenge', 'ip_range'=>'managed_challenge', 'ip'=>'managed_challenge']  // для стран нецелевого трафика
 ];
-?>
